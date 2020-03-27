d8ab8998010c1efb0f2d3f9c436d102fabf485fd
Maylyn Talampas <maylynt@surefiresocial.com>
2020-03-27 19:44:04 +0800

    Question6 Updates

--- app/Http/Controllers/Question6Controller.php
+++ app/Http/Controllers/Question6Controller.php
@@ -4,18 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use DB;
+use App\Customer;
 
 class Question6Controller extends Controller
 {
     public function solution()
     {
         $topSpender = $this->getTopSpender();
+        $adamsOrders = $this->getAdamsOrderItems();
 
         return array(
             'Is this a good database design? Why?' => 'No, because it can still be normalized.',
             'If your answer to question 1 is no, what can you propose to improve the
             design' => 'We can normalize the tables and update the relationships. I applied the proposed design here. Please take a look at the migration files and models',
-            'Top Spender' => $topSpender
+            'Who is the top spender in this online store?' => $topSpender,
+            'What kind of fruits (item) did Adam buy so far?' => $adamsOrders
         );
     }
     
@@ -29,5 +32,11 @@ class Question6Controller extends Controller
                     ->get();
 
         return $customers[0];
+    }
+
+    public function getAdamsOrderItems()
+    {
+        $items = Customer::find(1)->orders->pluck('item');
+        return $items;
     }
 }
